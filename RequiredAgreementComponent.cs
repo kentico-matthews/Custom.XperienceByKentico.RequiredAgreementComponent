f9844150eb4c837182f513ee497b4f180b5df478
Matthew Sandstrom <MatthewS@kentico.com>
2023-05-01 16:25:57 -0400

    Add project files.

---
+++ RequiredAgreementComponent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Localization;
+using Kentico.Forms.Web.Mvc;
+using CustomFormComponent.RequiredAgreement;
+
+
+[assembly:RegisterFormComponent(RequiredAgreementComponent.IDENTIFIER, typeof(RequiredAgreementComponent), "Required agreement", Description = "Provides a checkbox that must be selected in order for the form submission to pass validation. Ideal for mandatory agreements that are necessary in order to process the form submission.", IconClass = "icon-cb-check-preview", ViewName = "~/RequiredAgreementComponent/_RequiredAgreement.cshtml")]
+
+namespace CustomFormComponent.RequiredAgreement
+{
+    /// <summary>
+    /// Represents a checkbox form component that can be marked as required.
+    /// </summary>
+    public class RequiredAgreementComponent : FormComponent<RequiredAgreementProperties,bool>
+    {
+        /// <summary>
+        /// Represents the <see cref="RequiredAgreementComponent"/> identifier.
+        /// </summary>
+        public const string IDENTIFIER = "RequiredAgreementComponent";
+
+
+        /// <summary>
+        /// Represents the input value in the resulting HTML.
+        /// </summary>
+        [BindableProperty]
+        public bool Value { get; set; }
+
+
+        /// <summary>
+        /// Gets name of the <see cref="Value"/> property.
+        /// </summary>
+        public override string LabelForPropertyName => nameof(Value);
+
+
+        /// <summary>
+        /// Gets the <see cref="Value"/>.
+        /// </summary>
+        public override bool GetValue()
+        {
+            return Value;
+        }
+
+
+        /// <summary>
+        /// Sets the <see cref="Value"/>.
+        /// </summary>
+        public override void SetValue(bool value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Performs validation of the requireable check box component.
+        /// </summary>
+        /// <param name="validationContext">The validation context.</param>
+        /// <returns>A collection that holds failed-validation information.</returns>
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            //if the checkbox is not checked
+            if(!Value)
+            {
+                string errorMessage = string.IsNullOrEmpty(Properties.ErrorMessage) ? "This value must be selected to submit the form." : Properties.ErrorMessage;
+                errors.Add(new ValidationResult(errorMessage));
+            }
+
+            return errors;
+        }
+    }
+}
---
+++ RequiredAgreementProperties.cs
@@ -0,0 +1,71 @@
+﻿using CMS.DataEngine;
+using Kentico.Forms.Web.Mvc;
+using Kentico.Xperience.Admin.Base.FormAnnotations;
+
+namespace CustomFormComponent.RequiredAgreement
+{
+
+    public class RequiredAgreementProperties : FormComponentProperties<bool>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckBoxProperties"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The constructor initializes the base class to data type <see cref="FieldDataType.Boolean"/>.
+        /// </remarks>
+        public RequiredAgreementProperties() : base(FieldDataType.Boolean)
+        {
+        }
+
+
+        /// <summary>
+        /// Gets or sets the default value of the form component and underlying field.
+        /// </summary>
+        [DefaultValueEditingComponent(CheckBoxComponent.IDENTIFIER)]
+        public override bool DefaultValue
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Represents the input value in the resulting HTML.
+        /// </summary>
+        [TextInputComponent(Label = "{$kentico.formbuilder.component.checkbox.properties.text$}")]
+        // The following attributes are for backwards compatibility (mix mode):
+        [EditingComponent(TextInputComponent.IDENTIFIER, Label = "{$kentico.formbuilder.component.checkbox.properties.text$}")]
+        public string Text
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Represents a custom error message overriding the default set by the requiredagreementcomponent.notchecked resource string.
+        /// </summary>
+        [TextInputComponent(Label = "Error message", Tooltip = "A custom error message to display when the checkbox is not selected. Can include localization macros.")]
+        // The following attributes are for backwards compatibility (mix mode):
+        [EditingComponent(TextInputComponent.IDENTIFIER, Label = "Error message",Tooltip = "A custom error message to display when the checkbox is not selected.")]
+        public string ErrorMessage
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Represents whether the form component is required.
+        /// </summary>
+        /// <remarks>
+        /// Assigning no editing control means the required checkbox will not be visible.
+        /// This control will always behave as if it is required regardless of this property, but returning true here will make the property consistent for anyone inspecting the component properties while debugging.
+        /// </remarks>
+        public override bool Required 
+        {
+            get => true;
+            set => base.Required = value; 
+        }
+    }
+}
